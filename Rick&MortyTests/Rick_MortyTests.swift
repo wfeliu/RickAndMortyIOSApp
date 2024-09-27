0f3aef1a74b5fefb03aa92b7bfc178069d705282
Walter <wcarias@asu.edu>
2024-09-26 22:13:51 -0700

    Initial Commit

---
+++ Rick&MortyTests/Rick_MortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyTests.swift
+//  Rick&MortyTests
+//
+//  Created by Walter Feliu on 9/26/24.
+//
+
+import Testing
+@testable import Rick_Morty
+
+struct Rick_MortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Rick&MortyUITests/Rick_MortyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rick_MortyUITestsLaunchTests.swift
+//  Rick&MortyUITests
+//
+//  Created by Walter Feliu on 9/26/24.
+//
+
+import XCTest
+
+final class Rick_MortyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
